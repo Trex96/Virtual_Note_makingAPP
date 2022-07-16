e268d225cbc9b7a1c15cabc0a35a3d9b2202efbf
Bikash Marandi <jarvis7297@gmail.com>
2022-07-17 04:04:29 +0530

    CORs

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -2,7 +2,16 @@ import { Bucket, Table } from "@serverless-stack/resources";
 
 export function StorageStack({ stack, app }) {
  
-    const bucket = new Bucket(stack, "Uploads");
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
     const table = new Table(stack, "Notes", {
         fields: {
         userId: "string",
